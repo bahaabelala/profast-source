e64f47bff334d1e4b39c39a73e535fd4beb0b0fe
Bahaa Mohamed <bahaabelala15@gmail.com>
2023-10-09 00:29:24 +0300

    Fixing a bug

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,55 +1,55 @@
-import React from 'react';
-// import Head from 'next/head';
-import styles from '../../styles/projects.module.css';
+// import React from 'react';
+// // import Head from 'next/head';
+// import styles from '../../styles/projects.module.css';
 
-// import SideBar from '../components/ui/SideBar/SideBar';
-// import SideBarButton from '../components/ui/SideBarButton/SideBarButton';
-// import TasksContainer from '../components/ui/TasksContainer/TasksContainer';
-// import TaskBody from '../components/ui/TaskBody/TaskBody';
-// import TaskActForm from '../components/ui/TaskActForm/TaskActForm';
+// // import SideBar from '../components/ui/SideBar/SideBar';
+// // import SideBarButton from '../components/ui/SideBarButton/SideBarButton';
+// // import TasksContainer from '../components/ui/TasksContainer/TasksContainer';
+// // import TaskBody from '../components/ui/TaskBody/TaskBody';
+// // import TaskActForm from '../components/ui/TaskActForm/TaskActForm';
 
 
-class Projects extends React.Component {
+// class Projects extends React.Component {
 
-	render() {
-		return (
-			<div className={styles.ProjectsPage}>
-				{/*<Head>
-					<title>Projects</title>
-					<meta name="description" content="Make your projects' plan in the way you like to be" />
-				</Head>
+// 	render() {
+// 		return (
+// 			<div className={styles.ProjectsPage}>
+// 				{/*<Head>
+// 					<title>Projects</title>
+// 					<meta name="description" content="Make your projects' plan in the way you like to be" />
+// 				</Head>
 
-				<SideBar addBtnText="New Project">
-					<SideBarButton text="Productivity" active />
-					<SideBarButton text="Online Store" />
-					<SideBarButton text="Portfolio" />
-				</SideBar>
+// 				<SideBar addBtnText="New Project">
+// 					<SideBarButton text="Productivity" active />
+// 					<SideBarButton text="Online Store" />
+// 					<SideBarButton text="Portfolio" />
+// 				</SideBar>
 
-				<TasksContainer>
-					<TaskBody>
-				        <h3>Task-1 Title</h3>
-				        <p>Tempor occaecat nostrud et proident in cillum ullamco voluptate culpa ullamco excepteur consequat.</p>
-			        </TaskBody>
-			        <TaskBody>
-				        <h3>Task-2 Title</h3>
-				        <p>Tempor occaecat nostrud et proident in cillum ullamco voluptate culpa ullamco excepteur consequat.</p>
-			        </TaskBody>
-			        <TaskBody>
-				        <h3>Task-3 Title</h3>
-				        <p>Tempor occaecat nostrud et proident in cillum ullamco voluptate culpa ullamco excepteur consequat.</p>
-			        </TaskBody>
-			        <TaskBody completed>
-				        <h3>Task-4 Title</h3>
-				        <p>Tempor occaecat nostrud et proident in cillum ullamco voluptate culpa ullamco excepteur consequat.</p>
-			        </TaskBody>
+// 				<TasksContainer>
+// 					<TaskBody>
+// 				        <h3>Task-1 Title</h3>
+// 				        <p>Tempor occaecat nostrud et proident in cillum ullamco voluptate culpa ullamco excepteur consequat.</p>
+// 			        </TaskBody>
+// 			        <TaskBody>
+// 				        <h3>Task-2 Title</h3>
+// 				        <p>Tempor occaecat nostrud et proident in cillum ullamco voluptate culpa ullamco excepteur consequat.</p>
+// 			        </TaskBody>
+// 			        <TaskBody>
+// 				        <h3>Task-3 Title</h3>
+// 				        <p>Tempor occaecat nostrud et proident in cillum ullamco voluptate culpa ullamco excepteur consequat.</p>
+// 			        </TaskBody>
+// 			        <TaskBody completed>
+// 				        <h3>Task-4 Title</h3>
+// 				        <p>Tempor occaecat nostrud et proident in cillum ullamco voluptate culpa ullamco excepteur consequat.</p>
+// 			        </TaskBody>
 
-			        <TaskActForm />
+// 			        <TaskActForm />
 
-				</TasksContainer>*/}
+// 				</TasksContainer>*/}
 
-			</div>
-		);
-	}
-}
+// 			</div>
+// 		);
+// 	}
+// }
 
-export default Projects;
+// export default Projects;
